8841e808af4cf74b84ead24ff0e88f32a713a5ce
Kyler Riggs <100742516+KylerRiggs@users.noreply.github.com>
2022-11-28 19:23:18 -0500

    Update to 280 characters

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -42,7 +42,7 @@ export const NewPostForm = () => {
         value={content}
         placeholder="New post..."
         minLength={2}
-        maxLength={100}
+        maxLength={280}
         onChange={(event) => setContent(event.target.value)}
         className="rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
       />
