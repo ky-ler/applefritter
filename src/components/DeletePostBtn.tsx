ff43416bc84c3df306165bdbf7a52c3926e2d402
Kyler Riggs <100742516+KylerRiggs@users.noreply.github.com>
2022-12-10 22:48:28 -0500

    Confirm delete post

--- src/components/DeletePostBtn.tsx
+++ src/components/DeletePostBtn.tsx
@@ -1,5 +1,7 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { useSession } from "next-auth/react";
-import { FiTrash } from "react-icons/fi";
+import { BiEdit } from "react-icons/bi";
+import { FiTrash, FiX } from "react-icons/fi";
 import { trpc } from "../utils/trpc";
 
 export const DeletePostBtn = ({
@@ -39,36 +41,46 @@ export const DeletePostBtn = ({
 
   return (
     <>
-      <button
-        onClick={() => {
-          deletePost.mutate({ postId: postId });
-        }}
-        className="flex items-center align-top text-2xl"
-      >
-        {deletePost.isLoading ? (
-          <div>
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 animate-spin fill-emerald-400 align-bottom text-neutral-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        ) : (
-          <FiTrash />
-        )}
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button className="align-bottom text-2xl duration-300 active:-translate-y-1 active:transition-transform disabled:active:translate-y-0">
+            <FiTrash />
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 backdrop-blur-sm" />
+          <Dialog.Content className="fixed top-1/2 left-1/2 max-h-[85vh] w-[90vw] max-w-md -translate-y-1/2 -translate-x-1/2 rounded-md border-2 border-zinc-800 bg-neutral-900 p-6">
+            <Dialog.Title className="mb-4 font-semibold">
+              Are you sure you want to delete?
+            </Dialog.Title>
+
+            {/* <fieldset className="mb-4 flex gap-5"> */}
+            <div className="flex flex-col items-center space-y-4"></div>
+
+            <div className="flex justify-end">
+              <Dialog.Close asChild>
+                <button
+                  onClick={() => {
+                    deletePost.mutate({ postId: postId });
+                  }}
+                  className="mt-2 flex cursor-pointer items-center justify-center rounded-md border-2 border-red-800 p-2 transition-colors hover:border-red-600 focus:outline-none active:border-red-600 active:bg-neutral-800 disabled:cursor-not-allowed disabled:hover:border-red-800 disabled:active:bg-transparent"
+                >
+                  Delete
+                </button>
+              </Dialog.Close>
+            </div>
+            <Dialog.Close asChild>
+              <button
+                className="absolute top-3 right-3 inline-flex h-6 w-6 items-center justify-center rounded-full"
+                aria-label="Close"
+              >
+                <FiX />
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+      {/* <span className="ml-1">{postReplies.length}</span> */}
     </>
   );
 };
